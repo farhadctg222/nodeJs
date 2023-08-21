67d2a829e3f6a8696aa0e1bf0f2d29cf26916f54
Your Name <farhadctg222@gmail.com>
2023-08-21 20:57:52 +0600

    d

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
-const users = ["farhad","rakib","somaya","susmita"]
+const bodyParser = require('body-parser')
+app.use(cors())
 
+app.use(bodyParser.json())
+
+
+
+
+
+
+const users = ["farhad","rakib","somaya","susmita"]
 app.get('/mongo/friuts',(req,res)=>{
   const friuts={
     name:'fruts',
@@ -14,5 +24,10 @@ app.get('/users/:id',(req,res)=>{
   const userId = req.params.id
   const userName = users[userId]
   res.send({userId,userName})
+})
+app.post('/adduser',(req,res)=>{
+  const eee = req.body
+  eee.id = 55
+  res.send(eee)
 })
 app.listen(4000,console.log('this is my first node express js 4000'))
